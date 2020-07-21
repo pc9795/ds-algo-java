056766b6c95b223c5ef67f165c08fab7b1905663
Prashant <prashantchaubey9795@gmail.com>
2020-07-21 01:16:59 +0100

    Added Cartesian tree

---
+++ notes/gfg/ds/8_advanced/cartesian_tree.md
@@ -0,0 +1,5 @@
+1. The tree obeys in the min (or max) heap property
+2. An inorder traversal of the nodes yields the values in the same order in which they appear in the initial sequence.
+3. Not a height-balanced tree.
+4. Cartesian tree of a sequence of distinct numbers is always unique.
+
---
+++ src/main/java/gfg/ds/advanced/CartesianTree.java
@@ -0,0 +1,103 @@
+package gfg.ds.advanced;
+
+/**
+ * Created By: Prashant Chaubey
+ * Created On: 21-07-2020 00:02
+ **/
+public class CartesianTree {
+    private CartesianTreeNode root;
+    private CartesianTreeNode rightMost;
+
+    public CartesianTreeNode getRoot() {
+        return root;
+    }
+
+    public CartesianTreeNode getRightMost() {
+        return rightMost;
+    }
+
+    public CartesianTree insert(int... values) {
+        for (int value : values) {
+            insert(value);
+        }
+        return this;
+    }
+
+    /**
+     * t=O(n*log n); On average
+     */
+    public CartesianTree insert(int data) {
+        if (isEmpty()) {
+            root = rightMost = new CartesianTreeNode(data);
+            return this;
+        }
+
+        CartesianTreeNode ancestor = rightMost;
+        for (; ancestor != null; ) {
+            if (ancestor.data > data) {
+                CartesianTreeNode ancestorRightChild = ancestor.right;
+
+                CartesianTreeNode newNode = new CartesianTreeNode(data);
+                newNode.parent = ancestor;
+                ancestor.right = newNode;
+
+                newNode.left = ancestorRightChild;
+                if (ancestorRightChild != null) {
+                    ancestorRightChild.parent = newNode;
+                }
+                break;
+            }
+            ancestor = ancestor.parent;
+        }
+
+        if (ancestor == null) {
+            CartesianTreeNode newNode = new CartesianTreeNode(data);
+            newNode.left = root;
+            root.parent = newNode;
+            root = newNode;
+            rightMost = newNode;
+            return this;
+        }
+
+        if (ancestor == rightMost) {
+            rightMost = rightMost.right;
+        }
+
+        return this;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public static class CartesianTreeNode {
+        private int data;
+        private CartesianTreeNode left;
+        private CartesianTreeNode right;
+        private CartesianTreeNode parent;
+
+        public CartesianTreeNode(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public CartesianTreeNode getLeft() {
+            return left;
+        }
+
+        public CartesianTreeNode getRight() {
+            return right;
+        }
+
+        @Override
+        public String toString() {
+            return "CartesianTreeNode{" +
+                    "data=" + data +
+                    '}';
+        }
+    }
+
+}
